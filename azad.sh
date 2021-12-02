c3ef45cb52aff50b7303b6247d7bf86de32b0b7a
AZADAYAZ <95386329+AZADAYAZ@users.noreply.github.com>
2021-12-02 03:45:45 +0300

    Create azad.sh

---
+++ azad.sh
@@ -0,0 +1,85 @@
+#!Script  EG HACK
+# EG HACK TARAFINDAN Oluşturuldu
+# Yaratıcı bir insan olun, kopyala yapıştır yapmayın
+
+###################################
+cyan='\e[0;36m'
+
+lightgreen='\e[1;32m'
+
+red='\e[1;31m'
+
+yellow='\e[1;33m'
+###################################
+
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[31m\
+           ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+           ██░▄▄▄██░▄▄░████░██░█░▄▄▀██░▄▄▀██░█▀▄
+           ██░▄▄▄██░█▀▀████░▄▄░█░▀▀░██░█████░▄▀█
+           ██░▀▀▀██░▀▀▄████░██░█░██░██░▀▀▄██░██░
+           ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+echo " "  
+echo -e  "                  \e[1m\e[32m
+▂▃▄▅▆▇▓▒░Kod BAY EG TARAFINDAN OLUŞTU[31mN17R0 \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo "                      -------------------------"
+echo -e  "            \e[1m\e[32m--------->[\e[31mSAYGILARLA\e[1m\e[32m]<----------"  
+echo " "
+echo ""
+sleep 8.0
+pkg install mpv -y
+termux-setup-storage
+cd  /data/data/com.termux/files/usr/etc
+
+rm bash.bashrc
+
+ls
+
+cd $HOME
+
+ls 
+
+cd AZADAYAZ
+
+ls
+
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+
+ls
+
+cd $HOME
+
+ls
+
+cd AZADAYAZ
+
+ls
+
+cp scifi.mp3 /sdcard
+
+clear
+bash packages.sh
+clear
+sleep 4.0
+bash ins.sh 
+sleep 5.0
+clear
+sleep 5.0
+bash thanks.sh 
+sleep 2.0 
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "             "TERMUXTAN Çıkış Yapıp 5 saniye Bekleyip Giriş Yapın |lolcat 
+echo " "
+echo " "
+sleep 3.0
+cd $HOME
