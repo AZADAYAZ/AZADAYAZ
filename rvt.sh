3f58fc55728eb9546c5e24fa3e109328c764fa38
AZADAYAZ <95386329+AZADAYAZ@users.noreply.github.com>
2021-12-02 03:31:24 +0300

    Create rvt.sh

---
+++ rvt.sh
@@ -0,0 +1,49 @@
+#!Script  author N117R0
+# EG HACK TARAFINDAN Oluşturuldu
+# Yaratıcı bir insan olun, kopyala yapıştır yapmayın
+
+###################################
+cyan='\e[0;36m'
+
+lightgreen='\e[1;32m'
+
+red='\e[1;31m'
+
+yellow='\e[1;33m'
+###################################
+
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[31m\
+           
+             ___   ___        _  _  ___   ___  _  __
+            | __| / __|      | || |/   \ / __|| |/ /
+            | _| | (_ |      | __ || - || (__ |   < 
+            |___| \___|      |_||_||_|_| \___||_|\_\
+
+echo " "  
+echo -e  "                  \e[1m\e[32m
+▂▃▄▅▆▇▓▒░Kod BAY EG TARAFINDAN OLUŞTU[31mN17R0 \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo "                      -------------------------"
+echo -e  "            \e[1m\e[32m--------->[\e[31myoutube.com/noobhackers\e[1m\e[32m]<----------"  
+echo " "
+echo ""
+echo "                           geri dönüş için son 10 saniye"
+cd $HOME
+cd T-LOAD
+cd revert 
+mv bash.bashrc $HOME 
+cd  /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+cd $HOME
+
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+echo "             "TERMUXTAN ÇIKIP 5 SANİYE BEKLEYİP GİRİŞ YAPIN |lolcat 
+echo " "
+echo  "            Geri Dönüş Başarılı Şimdi Çıkış Yapabilirsin"
+sleep 3.0
+cd $HOME
